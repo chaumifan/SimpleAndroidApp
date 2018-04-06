25adf64cd6c0457ff30650a6a231ef3147387015
navsaini <nsaini@utexas.edu>
2018-04-06 16:28:07 -0500

    current location and error message

--- app/src/main/java/edu/kevinchauutexas/simpleandroidapp/MapsActivity.java
+++ app/src/main/java/edu/kevinchauutexas/simpleandroidapp/MapsActivity.java
@@ -1,17 +1,23 @@
 package edu.kevinchauutexas.simpleandroidapp;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -19,12 +25,19 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
 
 import java.io.IOException;
 import java.util.LinkedList;
@@ -36,9 +49,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private Queue<String> history;
     private int curSize;
-    private final int MAXHISTORYSIZE = 10;
+    private static final int MAXHISTORYSIZE = 10;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private Location mLastLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
+
+
+    private void setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +73,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         history = new LinkedList<>();
         curSize = 0;
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        setUpMap();
         configureButton();
     }
 
@@ -76,19 +101,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            if (addressList.isEmpty()) {
+                AlertDialog alertDialog = new AlertDialog.Builder(MapsActivity.this).create();
+                alertDialog.setTitle("Error");
+                alertDialog.setMessage("Invalid Location. Please Try Again.");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+                locationSearch.setText("");
+                return;
+            }
             mMap.clear();
             Address address = addressList.get(0);
-
             System.out.println(address);
             LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
             mMap.addMarker(new MarkerOptions().position(latLng).title(address.getFeatureName()));
             mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         }
         hideKeyboard(view);
+        locationSearch.setText("");
     }
+
     public void displayHistory(View view) {
         String historyStr = "";
-        for(String s : history) {
+        for (String s : history) {
             historyStr = s + "\n" + historyStr;
         }
         Intent intent = new Intent(this, HistoryActivity.class);
@@ -102,7 +142,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void hideKeyboard(View view) {
-        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
@@ -121,13 +161,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        InputMethodManager imm = (InputMethodManager)getSystemService(Context.
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.
                 INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
         return true;
@@ -139,9 +179,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         myLocation.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                ed.setText("Austin");
+            public void onClick(View v) {
+                System.out.println("onclick called");
+                getCurrentLocation();
             }
         });
+
     }
+
+   private void getCurrentLocation() {
+        try {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                mMap.clear();
+                                LatLng currLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                mMap.addMarker(new MarkerOptions().position(currLocation).title("current location"));
+                                mMap.moveCamera(CameraUpdateFactory.newLatLng(currLocation));
+                            } else {
+                                System.out.println("location is null");
+                            }
+                        }
+                    })
+            .addOnFailureListener(this, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    System.out.println(e);
+                }
+            });
+        } catch (SecurityException e) {
+            System.out.println("permission check failed");
+        }
+
+   }
 }
